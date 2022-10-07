9182b5c34fd28aac0b7a0fb28c260108756bd651
Simon <simon.aeby@laendrun.ch>
2022-10-07 21:59:58 +0200

    added the middlewares file...

---
+++ backend/routes/middlewares/index.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken');
+const { unauthorized } = require("../lib/utils");
+
+function checkTokenSetUser(req, res, next) {
+  // this middleware checks if the Authorization header is set
+  // if the Authorization exists and contains a token
+  // sets req.user as the payload from the decoded token
+    const authHeader =  req.get('Authorization');
+    if (authHeader){
+      const token = authHeader.split(' ')[1];
+      if (token){
+        jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
+          if (error) {
+            console.error(error);
+            unauthorized(res, next);
+          }
+          req.user = payload;
+          next();
+        });
+      } else {
+        next();
+      }
+    }
+    else {
+      next();
+    }
+  }
+
+  function isLoggedIn(req, res, next) {
+    if (req.user) {
+      next();
+    } else {
+      unauthorized(res, next);
+    }
+  }
+
+  function isAdmin(req, res, next) {
+    if (req.user.role_id == 9) {
+      next();
+    } else {
+      unauthorized(res, next);
+    }
+  }
+
+  function isOwner(req, res, next) {
+    if (req.user.artist_id == req.params.id) {
+      next();
+    } else {
+      unauthorized(res, next);
+    }
+  }
+
+  function isOwnerOrAdmin(req, res, next) {
+    if (req.user.artist_id == req.params.id || req.user.role_id == 9) {
+      next();
+    } else {
+      unauthorized(res, next);
+    }
+  }
+
+  module.exports = {
+    checkTokenSetUser,
+    isLoggedIn,
+    isAdmin,
+    isOwner,
+    isOwnerOrAdmin,
+  }
